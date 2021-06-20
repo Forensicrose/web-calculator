d0b5e28d54c66a313a1b80dbeb62b14e3afd2bf7
janeishahu <janeishahu@aol.com>
2021-06-20 16:40:30 -0400

    added css

--- calculator.js
+++ calculator.js
@@ -1,5 +1,10 @@
 function calculate() {
   const a = Number(document.getElementById("V1").value);
   const b = Number(document.getElementById("V2").value);
-  document.getElementById("result").innerHTML = (a+b);
+  document.getElementById("result").innerHTML = (a + b);
 }
+
+
+
+
+
