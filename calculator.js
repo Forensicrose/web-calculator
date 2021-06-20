ee0bfd4446d14589141d9d98c2ccac8fdcf69806
janeishahu <janeishahu@aol.com>
2021-06-20 16:18:36 -0400

    added const and fixed button

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
-Function (calculate) {}
-
-
-
-
+function calculate() {
+  const a = Number(document.getElementById("V1").value);
+  const b = Number(document.getElementById("V2").value);
+  document.getElementById("result").innerHTML = (a+b);
+}
